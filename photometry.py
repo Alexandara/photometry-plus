d30d4ccbc9e1bd533e607d05bffd54a5dadc57dd
Alexis Tudor <alexisrenee1@gmail.com>
2020-11-10 09:08:00 -0800

    Added email and settings support

--- photometry.py
+++ photometry.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9928dcec83a7b3155edd37fbacbeac24356b1e3b4bdd96cc5751f9e9923eeab1
-size 81815
+oid sha256:7ac7e0c0d946e374e685bfd61a0fd4b9b5aa3894a614098d25a83911302d1fca
+size 81586
--- photometryuserinterface.py
+++ photometryuserinterface.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a2acbaafdeb1e612d148b3b229a11bc5f20c69c6db5477624a9c3a54dbbc8ca7
-size 95457
+oid sha256:1f6c898b61e3729fb14f8d38b03e1f7b1a852b20f27ec2a51685abf44f755353
+size 97351
