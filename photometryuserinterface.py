ca3ccf3b0392f21df9b1bb673a1f8fc8b69f24ad
Alexis Tudor <alexisrenee1@gmail.com>
2020-12-02 08:00:03 -0800

    Minor fixes

--- photometryuserinterface.py
+++ photometryuserinterface.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d030cd5ec20e0bbd9f49626e40a502d0edb5b01e3291a08573ef95fb3f54b2e9
-size 131302
+oid sha256:be7709d77f3f1f00f52071e21a6709321fa3b65d8e6798c0d8e9ea177f35e408
+size 131462
