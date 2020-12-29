b8ebfecc8f00cf3debf401e14ebefe2af2cd9470
Alexis Tudor <alexisrenee1@gmail.com>
2020-12-29 15:46:51 -0800

    December updates to user interface

--- photometryuserinterface.py
+++ photometryuserinterface.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:be7709d77f3f1f00f52071e21a6709321fa3b65d8e6798c0d8e9ea177f35e408
-size 131462
+oid sha256:719d75f1fa3368c5107fdea2f6f02d64b47b1e911d09e94dfd470c90739b02e0
+size 136721
