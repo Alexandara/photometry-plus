39004c3f8fa434ce68acf02b7a4eb3af0d9edb1e
Alexis Tudor <alexisrenee1@gmail.com>
2021-01-28 15:47:08 -0800

    User Study testing complete

--- photometry.py
+++ photometry.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5386d0bbd982bd22288bcfebec5d8176a9b0d41cfcca7b30f10fb8ed78ce429f
-size 81650
+oid sha256:ff29fa3104b76eedd30eddb97b520a8c957e73a3e24b31813fef11cff9e010a9
+size 82168
