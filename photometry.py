76e478d285329387abe328a1e5fba693bbf4652e
Alexis Tudor <alexisrenee1@gmail.com>
2021-02-15 09:06:24 -0800

    Added outputDetailedInformation setting

--- photometry.py
+++ photometry.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ff29fa3104b76eedd30eddb97b520a8c957e73a3e24b31813fef11cff9e010a9
-size 82168
+oid sha256:369eeb29e5ca47741f7da1d5b2d81bf057dffe1e5af398c06704dfef881e9173
+size 88372
