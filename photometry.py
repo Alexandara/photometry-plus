a339f2baec20c1010bbb5d5e56e344e5ef0c4293
Alexis Tudor <alexisrenee1@gmail.com>
2021-02-15 14:37:28 -0800

    Added error setting to use error stars for checking and removed contact page feature

--- photometry.py
+++ photometry.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3fd8832ccf47b621f6a255824b398d481facfe8f8f5b5b3cdf7046fc6e3370b1
-size 89790
+oid sha256:10a47881794b7999d8f09d031334198c4d775e3625fdef1d1079c54b761262df
+size 92343
