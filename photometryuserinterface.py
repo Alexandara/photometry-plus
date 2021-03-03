b15717063efbc6907d3175543ca5fcdd7eec7180
Alexis Tudor <alexisrenee1@gmail.com>
2021-03-03 09:44:04 -0800

    Added detection for error stars error method problems

--- photometryuserinterface.py
+++ photometryuserinterface.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ebe708957798c429cf8ede2914d7d99e60db70472932f021db691a7e649e34f2
-size 138590
+oid sha256:590aa2412f852f2b5d8974d18f75ea28978274a6c9ec6ef4f3bb0d3ec8da260a
+size 139049
