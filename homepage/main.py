c8f0bfe6b80ba0817c1af9736b9a044f57f5c8e7
Alexis Tudor <alexisrenee1@gmail.com>
2021-04-23 13:10:13 -0700

    LFS removed

--- homepage/main.py
+++ homepage/main.py
@@ -1,3 +1,28 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:37b62d5661f815eda47e1d522220831e343f5074dbd0c67d5f02437545ff81b6
-size 697
+# This Python file uses the following encoding: utf-8
+import sys
+import os
+
+
+from PySide2.QtWidgets import QApplication, QMainWindow
+from PySide2.QtCore import QFile
+from PySide2.QtUiTools import QUiLoader
+
+
+class homepage(QMainWindow):
+    def __init__(self):
+        super(homepage, self).__init__()
+        self.load_ui()
+
+    def load_ui(self):
+        loader = QUiLoader()
+        path = os.path.join(os.path.dirname(__file__), "homepage.ui")
+        ui_file = QFile(path)
+        ui_file.open(QFile.ReadOnly)
+        loader.load(ui_file, self)
+        ui_file.close()
+
+if __name__ == "__main__":
+    app = QApplication([])
+    widget = homepage()
+    widget.show()
+    sys.exit(app.exec_())
